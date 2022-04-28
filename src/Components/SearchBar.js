3d34a9315d039e39a4951e65631054ad8fb7e9e7
vibha singh <svibha081@gmail.com>
2022-04-28 23:38:22 +0530

    fetch data from giphy api

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -2,10 +2,29 @@ import React, { useState } from "react";
 
 export const SearchBar = () => {
     const [searchText, setSearchText] = useState("");
+    const [searchResults, setSearchResults] = useState([]);
 
+    // Giphy API call
+    const apiCall = async () => {
+        let giphyAPI = `https://api.giphy.com/v1/gifs/search?q=${searchText}&api_key=${process.env.REACT_APP_GIPHY_KEY}&limit=5`;
+        fetch(giphyAPI)
+        .then(response => {
+            return response.json();
+        })
+        .then(data=> {
+            console.log(data);
+            setSearchResults(data.data);
+
+        })
+        .catch(err => console.log(err));
+        
+    }
+ 
+    // Hot Search
     const handleChange = (e) => {
         setSearchText(e.target.value);
         console.log(e.target.value)
+        apiCall();
     }
 
     return <div>
